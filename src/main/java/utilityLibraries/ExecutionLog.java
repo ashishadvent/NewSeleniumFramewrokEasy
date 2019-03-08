848b0f33b595ef81e76cad32dcccc62e199c9dc6
akesarwa <akesarwa@NOI-5ZHVR72.gemalto.com>
2019-03-09 01:56:53 +0530

    ..

---
+++ src/main/java/utilityLibraries/ExecutionLog.java
@@ -0,0 +1,43 @@
+package utilityLibraries;
+
+import org.apache.log4j.Logger;
+
+import com.aventstack.extentreports.model.Log;
+
+public class ExecutionLog {
+	
+	private static Logger log=Logger.getLogger(Log.class.getName());
+	
+	// Need to create these methods, so that they can be called  
+	 
+	 public static void info(String message) {
+	 
+		 log.info(message);
+	 
+	 }
+	 
+	 public static void warn(String message) {
+	 
+		 log.warn(message);
+	 
+	 }
+	 
+	 public static void error(String message) {
+	 
+		 log.error(message);
+	 
+	 }
+	 
+	 public static void fatal(String message) {
+	 
+		 log.fatal(message);
+	 
+	 }
+	 
+	 public static void debug(String message) {
+	 
+		 log.debug(message);
+	 
+	 }
+
+}
